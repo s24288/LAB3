8e6c247db2a4d93090735312330eddffd4027739
Oktawiusz <93832086+s24288@users.noreply.github.com>
2022-05-15 18:38:54 +0200

    Update TestAuthor.java

--- Zad3.2/src/TestAuthor.java
+++ Zad3.2/src/TestAuthor.java
@@ -4,11 +4,11 @@ public class TestAuthor {
         Author author2 = new Author("Janusz", "Nowak", 30);
         Author author3 = new Author("Robert", "Wolski", 40);
 
-        author1.toString();
+        System.out.println(author1);
         author1.setSurname("Jagiełło");
-        author1.toString();
-        
-        author2.toString();
-        author3.toString();
+        System.out.println(author1);
+
+        System.out.println(author2);
+        System.out.println(author3);
     }
 }
